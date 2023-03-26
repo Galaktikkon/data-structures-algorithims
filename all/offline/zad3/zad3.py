cd193043ab565e6ade5fcff1f5f979017d3f1b09
Marek Malek <malekmarek56@gmail.com>
2023-03-27 00:03:58 +0200

    ind

--- all/offline/zad3/zad3.py
+++ all/offline/zad3/zad3.py
@@ -1,25 +1,67 @@
+# Marek Małek, 414880
+
 from zad3testy import runtests
 
-def strong_string(T):
-    # tu prosze wpisac wlasna implementacje
-    
-    curr_count=0
-    max_count=0
-    
-    for i in range(len(T)):
-        
-        curr_count=0
-        for j in range(len(T)):
-            if T[i]==T[j] or T[i]==T[j][::-1]:
-                curr_count+=1
-        if curr_count>len(T)//2:
-            return curr_count
-        max_count=max(curr_count,max_count)
-        
-    return max_count
+def convert(A,index):
+    return ord(A[index])-ord('a')
 
-# T = ["pies", "mysz", "kot", "kogut", "tok", "seip", "kot"]
+def reverseStrings(T):
+    
+    n=len(T)
+    for i in range(n):
+        if T[i]>T[i][::-1]:
+            T[i]=T[i][::-1]
+    
+    return T
+
+def extend(T):
+    
+    n=len(T)
+    
+    maxLen=-float('inf')
+    for i in T:
+        maxLen=max(maxLen,len(i))
+        
+    for i in range(n):
+        if len(T[i])<maxLen:
+            while len(T[i])<maxLen:
+                T[i]=chr(96)+T[i]
+    
+    return T
+       
+def countingSort(A,index):
+    
+    n=len(A)
+    
+    C=[0]*26
+    B=[0]*n
+    
+    for i in range(n):
+        C[convert(A[i],index)]+=1
+        
+    for i in range(1,26):
+        C[i]=C[i]+C[i-1]
+        
+    for i in range(n-1,-1,-1):
+        B[C[convert(A[i],index)]-1]=A[i]
+        C[convert(A[i],index)]-=1
+        
+    return B
+
+def radixSort(T,index):
+    pass
+
+def strong_string(T):
+    pass
+
+T = ["pies", "mysz", "kot", "kogut", "tok", "seip", "kot"]
+
+reverseStrings(T)
+extend(T)
+countingSort(T,4)
+print(T)
+
 
 # print(strong_string(T))
 # # zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( strong_string, all_tests=False )
+# runtests( strong_string, all_tests=False )
