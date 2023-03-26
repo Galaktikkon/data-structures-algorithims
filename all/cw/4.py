281b51633d1a290da5a54813f49034fb7f5bc272
Marek Malek <malekmarek56@gmail.com>
2023-03-26 16:52:40 +0200

    piontki

---
+++ all/cw/4.py
@@ -0,0 +1,14 @@
+#  1. Dana jest tablica n liczb ze zbioru [0,n^2-1], posortować jak najszybciej
+#  2. Dana jest tablica n liczb zawierająca O(logn) różnych wartości, posortować jak najszybciej
+#  3. Dana jest tablica n liczb, szukamy takich dwóch, które po posortowaniu byłyby koło siebie oraz ich różnica jest maksymalna.
+#  4. Dane sa dwa napisy, zaimplementowac funkcję sprawdzającą czy są anagramami.
+#  5. Mamy tablicę T rozmiaru n, T[i] zawiera się w {0,...,k-1}, chcemy najmniejszy podprzedział, który zawiera wszystkie kolory. 
+#  6. wartosć od 0-10^9-1, losowane są serie danych z tego przedziału, trzeba zliczyć ile w serii jest powtarzających się wartości.
+#  7. znalezienie najmniejszego wielokąta wypukłego, który obejmuje wszystkie punkty - problem otoczki wypukłej
+# ad 1.
+# radix sort po 1) %n i 2) //n
+# ad 2.
+# tablica tablic, zawierajaca informacje ile razy wystapila liczba w glownej tablicy, (wyszukiwanie binarne)jak jest to dodajemy, 
+# jak nie to wstawiamy, n razy wstawiwamy liczbe wyszukujac binarne log od logn liczb - nlog(logn)
+# ad 3. podział na n kubełków, i szukanie min max w zależności od sytuacji.
+# ad 5. dwoma wskaźnikami
--- all/wyklad/3.py
+++ all/wyklad/3.py
@@ -1,6 +1,6 @@
 # counting sort
 
-A=[2,0,3,1,1,2,3,2]
+A=[2,0,3,1,1,2,3,2,5,3,2,4,3,3,22,1,4,2,3,77,4,23,12,45,23,6,5,12,66,8,9,0,3,12,43,55,67,3,83,3,82,13,98,56,24,23,12,34,356,97,67,8,67]
 
 def countingSort(A,k):
     n=len(A)
@@ -20,12 +20,12 @@ def countingSort(A,k):
         
     return B
 
-print(countingSort(A,4))
+# print(countingSort(A,4))
 
 
 # RADIX SORT
 
-A=['kra','art','ort','kot','kit','ati','kil']
+# A=['kra','art','ort','kot','kit','ati','kil']
 
 # pomysl - counting sort po ostatnich literach, bo znamy przedział danych
 
@@ -40,5 +40,94 @@ A=['kra','art','ort','kot','kit','ati','kil']
 
 # oczekiwana złożoność - O(n)
 
+# MAGICZNE PIĄTKI
+
 # Magiczne Piątki - wybór k-tego elementu w czasue O(n) bez ryzyka "ukwadratowienia"
-# A - tab 
+# A - tablica n liczb, chcemy znaleźć liczbę, która po posortowaniu byłaby na k-tej pozycji.
+# 1. Podziel wejściową tablicę (A), na ceil(n/5) grup, po pięć elementów.
+# 2. Dla każdej z tych grup, znajdujemy medianę.
+# 3. Rekurencyjnie znajduję x - medianę wśród median naszych piątek.
+# 4. Wykonaj partition() używając x jako pivot.
+# 5. Jeśli x jest na pozycji k-tej, to zwróć x
+# 6. Jeśli x jest na pozycji dalszej niż k, to szukaj rekurencyjnie na lewej części tablicy
+# 7. W pp. w prawej części tablicy.
+
+import math
+
+def selectionSort(A):
+    n=len(A)
+    
+    for i in range(n):
+        
+        currMin=i 
+        
+        for j in range(i+1,n): 
+            
+            if A[currMin]>A[j]: 
+                currMin=j
+                
+        A[currMin],A[i]=A[i],A[currMin] 
+
+    return A
+
+def divIntoGroups(A):
+    n=len(A)
+    output=[[] for _ in range(math.ceil(n/5))]
+    
+    for i in range(n):
+        output[i//5].append(A[i])
+    
+    for i in range(len(output)):
+        output[i]=selectionSort(output[i])
+    
+    return output
+
+def partition(A,p,r):
+    
+    x=A[r] 
+           
+    i=p-1 
+    
+    for j in range(p,r): 
+        if A[j]<=x:
+            i+=1 
+            A[j],A[i]=A[i],A[j] 
+    A[r],A[i+1]=A[i+1],A[r]
+    
+    return i+1 
+
+def select(A,k):
+    
+    p=0
+    r=len(A)-1
+    q=partition(A,p,r)
+    
+    if q==k:
+        return A[q]
+    
+    elif q>k:
+        return select(A,k,p,q-1)
+        
+    elif q<k:
+        return select(A,k,q+1,r)
+
+def findMedian(A):
+    medians=[]
+    for i in range(A):
+        medians.append(A[i][len(A[i])//2])
+    n=len(medians)
+    return select(medians,n/2)
+
+def MagicFives(A,p,r):
+    
+    x=findMedian(A)
+           
+    i=p-1 
+    
+    for j in range(p,r): 
+        if A[j]<=x:
+            i+=1 
+            A[j],A[i]=A[i],A[j] 
+    A[r],A[i+1]=A[i+1],A[r]
+    
+    
