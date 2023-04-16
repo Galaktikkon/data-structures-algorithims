d759b959612ede3460fe56e2c65ea72c8a3314e0
Marek Malek <malekmarek56@gmail.com>
2023-04-16 18:11:08 +0200

    asd

---
+++ all/BiT/4 - grafy/5.py
@@ -0,0 +1,42 @@
+# Dany jest sejf, który odblokowuje się czterocyfrowym Pinem (0000-9999).
+# Pod wyświetlaczem znajduje się kilka przycisków z liczbami od 1 do 9999 - przykładowo (13, 223, 782, 3902).
+# Sejf ten działa inaczej niż normalny, wciśnięcie przycisku z liczbą powoduje dodanie liczby z przycisku do liczby na wyświetlaczu.
+# Jeżeli suma jest większa niż 9999, to pierwsza cyfra zosaje obcięta.
+
+# Jest tobie znany PIN oraz cyfry, które są aktualnei wyświetlane. Znajdź najkrótszą sekwencję naciśnięć przycisków,
+# która pozwoli ci odblokować sejf. Jeżeli taka sekwencja nie istnieje, zwróc None.
+
+from collections import deque
+
+def safe(T,p,s):
+    Q=deque()
+    n=10000
+    visited=[False  for _ in range(n)]
+    visited[s]=True
+    parent=[None for _ in range(n)]
+    parent[s]=None
+    d=[0 for _ in range(n)]
+    d[s]=0
+    entries=[p]
+    Q.append(s)
+    while Q:
+        u=Q.pop()
+        for v in T:
+            # if u==p:
+            #     # while parent[u]!=0:
+            #     #     entries.append(parent[u])
+            #     #     u=parent[u]
+            #     # entries.reverse()
+            #     return d[p]
+            if visited[(v+u)%n]==False:
+                visited[(v+u)%n]=True
+                d[(u+v)%n]=d[u]+1
+                parent[(v+u)%n]=u
+                Q.append((v+u)%n)
+                
+    return d[p]
+
+
+T=[13,14]
+
+print(safe(T,40,0))
