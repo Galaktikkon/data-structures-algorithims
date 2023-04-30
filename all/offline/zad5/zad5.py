7e13ffb9ebfbf98f7e7e2fbbc21b21cddfda6d5e
Marek Malek <malekmarek56@gmail.com>
2023-04-30 14:16:49 +0200

    asd

--- all/offline/zad5/zad5.py
+++ all/offline/zad5/zad5.py
@@ -1,14 +1,14 @@
 from zad5testy import runtests
+from math import inf
+from queue import PriorityQueue
 
-def Dijkstra(G,s,S):
+def Dijkstra(G,s,S,A):
     def relax(v,c,d,u):
         if d[v]>d[u]+c:
             d[v]=d[u]+c
             return True
         return False
     n=len(G)
-    from math import inf
-    from queue import PriorityQueue
     d=[inf for _ in range(n)]
     Q=PriorityQueue()
     d[s]=0
@@ -17,42 +17,28 @@ def Dijkstra(G,s,S):
     while not Q.empty():
         w,u=Q.get()
         if w==d[u]:
-            if not flag and u in S:
+            if not flag and A[u]:
                 for v in S:
                     if relax(v,0,d,u):
                         Q.put((d[v],v))
                 flag=True
-            for v,c in G[u]:
-                if relax(v,c,d,u):
-                    Q.put((d[v],v))
+            else:
+                for v,c in G[u]:
+                    if relax(v,c,d,u):
+                        Q.put((d[v],v))
     return d
 
 def spacetravel( n, E, S, a, b ):
     # tu prosze wpisac wlasna implementacje
-    from math import inf
     G=[[] for _ in range(n)]
     for i in range(len(E)):
         G[E[i][0]].append((E[i][1],E[i][2]))
         G[E[i][1]].append((E[i][0],E[i][2]))
-    # print(G)
-    # for i in range(len(S)):
-    #     for j in range(len(S)):
-    #         if j!=i:
-    #             G[S[i]].append((S[j],0))
+    A=[False for _ in range(n)]
+    for i in S: A[i]=True
     
-    dist=Dijkstra(G,a,S)
+    dist=Dijkstra(G,a,S,A)
     return dist[b] if dist[b]!=inf else None
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
 runtests( spacetravel, all_tests = True )
-
-# E = [(0,1, 5),
-# (1,2,21),
-# (1,3, 1),
-# (2,4, 7),
-# (3,4,13),
-# (3,5,16),
-# (4,6, 4),
-# (5,6, 1)]
-# S = [ 0, 2, 3 ]
-# print(spacetravel(7,E,S,1,5))
