ef520989894cba7920907c0d0a89f43a59486fd3
Marek Malek <malekmarek56@gmail.com>
2023-04-30 16:58:46 +0200

    asd

--- all/offline/zad5/zad5.py
+++ all/offline/zad5/zad5.py
@@ -5,19 +5,20 @@
 # tzn. maksymalna liczba skoków czasoprzetrzennych wynosi 1, inaczej droga byłaby nieefektywna, bo przechodząc drugi raz jakąś anomalią w 
 # innym kierunku, mogliśmy za pierwszym razem wybrać tę drogę, czyli istnieje optymalniejsza ścieżka. Realizowane jest to przez flagę, która 
 # pozwala na skorzystanie z anomalii tylko raz. W przypadku, gdy trafimy do planety w zagięciu (A[v]=True), patrzymy na inne planety w 
-# zagięciu (należące do S) i rozważamy drogi uzyskane przez skoki do tych planet. Na końcu algorytmu korzystamy z tablicy d, która zawiera 
-# odległości od wierzchołka "a" i sprawdzamy czy istnieje droga do "b", jeśli tak to ją zwracamy, inaczej zwracamy None.
+# zagięciu (należące do S) i rozważamy drogi uzyskane przez skoki do tych planet. Jeśli wyciągamy wierzchołek "b", to zwracamy tablicę "d".
+# Na końcu algorytmu korzystamy z tablicy d, która zawiera odległości od wierzchołka "a" i sprawdzamy 
+# czy istnieje droga do "b", jeśli tak to ją zwracamy, inaczej zwracamy None.
 
-# Szacowana złożoność obliczeniowa O(S + E + ElogV) 
+# Szacowana złożoność obliczeniowa O(S + E + ElogV)
 # - gdzie S liczba anomalii, E to liczba wejściowych krawędzi, a V to liczba wierzchołków grafu.
 
-# Szacowana złożoność pamięciowa O(S + V + E) 
+# Szacowana złożoność pamięciowa O(S + V + E)
 # - gdzie S liczba anomalii, E to liczba wejściowych krawędzi, a V to liczba wierzchołków grafu.
 from zad5testy import runtests
 from math import inf
 from queue import PriorityQueue
 
-def Dijkstra(G,s,S,A):
+def Dijkstra(G,s,S,A,b):
     def relax(v,c,d,u):
         if d[v]>d[u]+c:
             d[v]=d[u]+c
@@ -31,6 +32,8 @@ def Dijkstra(G,s,S,A):
     flag=False
     while not Q.empty():
         w,u=Q.get()
+        if u==b:
+            return d
         if w==d[u]:
             if not flag and A[u]:
                 for v in S:
@@ -52,7 +55,7 @@ def spacetravel( n, E, S, a, b ):
     A=[False for _ in range(n)]
     for i in S: A[i]=True
     
-    dist=Dijkstra(G,a,S,A)
+    dist=Dijkstra(G,a,S,A,b)
     return dist[b] if dist[b]!=inf else None
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
