ff8b644ac03a2673d41d89f2a4c7fb7cc8bbf75b
Marek Malek <malekmarek56@gmail.com>
2023-05-23 00:33:58 +0200

    offline6 unlucky

--- all/offline/zad6/tempCodeRunnerFile.py
+++ all/offline/zad6/tempCodeRunnerFile.py
@@ -1,1 +1,12 @@
-G
+def create_matrix(M):
+    n = len(M)
+    G = [[0 for _ in range(2*n+2)] for _ in range(2*n+2)]
+    for u in range(n):
+        for v in M[u]:
+            G[u][n+v] = 1
+    for i in range(n):
+        G[2*n][i] = 1
+    for i in range(n):
+        G[n+i][2*n+1] = 1
+
+    return G
