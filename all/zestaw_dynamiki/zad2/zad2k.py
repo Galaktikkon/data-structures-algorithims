6d5dd6cacb27f41ea9642c7f7bd45a85623e47ca
Marek Malek <malekmarek56@gmail.com>
2023-07-03 00:12:36 +0200

    3/12 wozniak done

--- all/zestaw_dynamiki/zad2/zad2k.py
+++ all/zestaw_dynamiki/zad2/zad2k.py
@@ -1,7 +1,32 @@
 from zad2ktesty import runtests
 
-def palindrom( S ):
-    #Tutaj proszę wpisać własną implementację
-    return ""
 
-runtests ( palindrom )
+def palindrom(S):
+    # Tutaj proszę wpisać własną implementację
+    n = len(S)
+    F = [[0 for _ in range(n)] for _ in range(n)]
+    max_pal = 1
+    max_i = 0
+    max_j = 0
+    for s in range(n):
+        i = 0
+        j = i+s
+        while j < n:
+            if i == j:
+                F[i][j] = 1
+            elif j-i == 1 and S[i] == S[j]:
+                F[i][j] = 2
+            else:
+                if S[i] == S[j] and F[i+1][j-1]:
+                    F[i][j] = F[i+1][j-1]+2
+            if max_pal < F[i][j]:
+                max_i = i
+                max_j = j
+                max_pal = F[i][j]
+            i += 1
+            j += 1
+
+    return S[max_i:max_j+1]
+
+
+runtests(palindrom)
--- all/zestaw_dynamiki/zad3/zad3k.py
+++ all/zestaw_dynamiki/zad3/zad3k.py
@@ -1,7 +1,32 @@
 from zad3ktesty import runtests
 
-def ksuma( T, k ):
-    #Tutaj proszę wpisać własną implementację
-    return 0
-    
-runtests ( ksuma )
+
+def ksuma(T, k):
+    # Tutaj proszę wpisać własną implementację
+    from math import inf
+    n = len(T)
+    F = [inf for _ in range(n)]
+    res = inf
+    for i in range(k):
+        F[i] = T[i]
+    for i in range(k, n):
+        for j in range(i-k, i):
+            F[i] = min(F[i], T[i]+F[j])
+    for i in range(n-k, n):
+        res = min(res, F[i])
+    return res
+
+
+T = [1, 2, 3, 4, 6, 15, 8, 7]
+k = 4
+
+
+def check(T, k):
+    F = ksuma(T, k)
+    print(F)
+
+
+# check(T, k)
+
+
+runtests(ksuma)
