3248e1ec5ac80c4aee58c1a76b794cd225895c4f
Marek Malek <malekmarek56@gmail.com>
2023-07-05 22:05:22 +0200

    wozniak set done

--- all/zestaw_dynamiki/zad10/zad10k.py
+++ all/zestaw_dynamiki/zad10/zad10k.py
@@ -1,9 +1,48 @@
 from zad10ktesty import runtests
-
-def dywany ( N ):
-    #Tutaj proszę wpisać własną implementację
-    return []
+from math import isqrt
+from math import inf
 
 
-runtests( dywany )
+def dywany(N):
+    # Tutaj proszę wpisać własną implementację
+    F = [-1 for _ in range(N+1)]
+    F[0] = 0
+    F[1] = 1
 
+    def rec_dyw(F, N):
+        res = inf
+
+        if N < 0:
+            return inf
+
+        if F[N] != -1:
+            return F[N]
+
+        if pow(isqrt(N), 2) == N:
+            F[N] = 1
+            return F[N]
+        else:
+            for i in range(1, isqrt(N)+1):
+                res = min(res, rec_dyw(F, N-pow(i, 2))+1)
+            if res != inf:
+                F[N] = res
+
+        return F[N]
+
+    rec_dyw(F, N)
+    l = N
+    out = []
+    while l > 0:
+        min_i = N
+        min_l = 1
+        for i in range(1, isqrt(N)+1):
+            if min_i > rec_dyw(F, l-pow(i, 2)):
+                min_i = rec_dyw(F, l-pow(i, 2))
+                min_l = i
+        out.append(min_l)
+        l -= pow(min_l, 2)
+
+    return out
+
+
+runtests(dywany)
--- all/zestaw_dynamiki/zad11/zad11k.py
+++ all/zestaw_dynamiki/zad11/zad11k.py
@@ -1,9 +1,37 @@
 from zad11ktesty import runtests
+from math import inf
+
+
+def prefix_sum(T):
+    n = len(T)
+    F = [0 for _ in range(n)]
+    F[0] = T[0]
+    for i in range(1, n):
+        F[i] = F[i-1]+T[i]
+    return F
+
 
 def kontenerowiec(T):
-    #Tutaj proszę wpisać własną implementację
+    # Tutaj proszę wpisać własną implementację
+    n = len(T)
+    p = sum(T)
+    F = [[-1 for _ in range(p+1)] for _ in range(n)]
+    L = prefix_sum(T)
 
-    return 0
+    def f(F, T, L, p, i):
 
-runtests ( kontenerowiec )
-    
+        if i == len(T)-1:
+            return abs((L[i]-p)-p)
+
+        if F[i][p] != -1:
+            return F[i][p]
+        F[i][p] = min(f(F, T, L, p+T[i], i+1), f(F, T, L, p, i+1))
+
+        return F[i][p]
+
+    f(F, T, L, 0, 0)
+
+    return F[0][0]
+
+
+runtests(kontenerowiec)
