c8716bfc7d81b3375af966aae4a028d73ac3ee9b
Marek Malek <malekmarek56@gmail.com>
2023-07-06 21:34:06 +0200

    morse dick

--- all/egaminy/probny_1/egzP1a/egzP1a_wz.py
+++ all/egaminy/probny_1/egzP1a/egzP1a_wz.py
@@ -26,17 +26,18 @@ M = [('A', '.-'), ('B', '-...'), ('C', '-.-.'), ('D', '-..'), ('E', '.'), ('F', 
 def titanic(W, M, D):
     # tutaj proszę wpisać własną implementację
     morsed = to_morse(W, M)
+    dic = {}
     n = len(morsed)
     m = len(D)
-    print(D)
+    for i in range(m):
+        dic[M[D[i]][1]] = len(M[D[i]][1])
     F = [n for _ in range(n+1)]
     F[0] = 1
     F[-1] = 0
     for i in range(1, n):
-        for j in range(m):
-            sign = M[D[j]][1]
-            if i-len(sign) >= -1 and morsed[i-len(sign)+1:i+1] == sign:
-                F[i] = min(F[i], F[i-len(sign)]+1)
+        for j in range(5):
+            if morsed[i-j+1:i+1] in dic:
+                F[i] = min(F[i], F[i-j]+1)
     return F[n-1]
 
 
