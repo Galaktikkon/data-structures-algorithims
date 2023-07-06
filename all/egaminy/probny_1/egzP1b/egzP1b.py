cd193ff500d729efeb45c4a621d7a8af80863455
Marek Malek <malekmarek56@gmail.com>
2023-07-06 23:01:22 +0200

    tourist

--- all/egaminy/probny_1/egzP1b/egzP1b.py
+++ all/egaminy/probny_1/egzP1b/egzP1b.py
@@ -1,9 +1,37 @@
 from egzP1btesty import runtests
+from math import inf
+
+
+def get_graph(E):
+    n = 0
+    for i in range(len(E)):
+        n = max(n, E[i][0], E[i][1])
+    G = [[] for _ in range(n+1)]
+    for edge in E:
+        G[edge[0]].append((edge[1], edge[2]))
+        G[edge[1]].append((edge[0], edge[2]))
+    return G
 
 
 def turysta(G, D, L):
     # tutaj proszę wpisać własną implementację
-    return 0
+    A = get_graph(G)
+    n = len(A)
+    T_d = [0 for _ in range(n)]
+    T_l = [0 for _ in range(n)]
+
+    for v in A[D]:
+        for u, c in A[v[0]]:
+            T_d[u] = c+v[1]
+    for v in A[L]:
+        for u, c in A[v[0]]:
+            T_l[u] = c+v[1]
+    path_weight = inf
+    for i in range(1, n-1):
+        if T_d[i] != 0 and T_l[i] != 0:
+            path_weight = min(path_weight, T_d[i]+T_l[i])
+
+    return path_weight
 
 
 G = [
@@ -16,5 +44,7 @@ G = [
 ]
 D = 0
 L = 6
+
+turysta(G, D, L)
 
 runtests(turysta)
