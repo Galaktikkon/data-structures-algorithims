39d223d12c5eee25bc9867b1764b01d6b211b06c
Marek Malek <malekmarek56@gmail.com>
2023-07-12 23:52:52 +0200

    snow done

--- all/egaminy/2021-2022/egz3/egzamin3_template/zad_A/egz3a.py
+++ all/egaminy/2021-2022/egz3/egzamin3_template/zad_A/egz3a.py
@@ -1,8 +1,32 @@
 from egz3atesty import runtests
 
-def snow( T, I ):
+T = 100
+I = [(3, 10), (0, 5), (20, 30), (25, 35), (26, 26)]
+
+
+def snow(T, I):
     # tu prosze wpisac wlasna implementacje
-    return 0
+    A = []
+    for i in range(len(I)):
+        if I[i][0] == I[i][1]:
+            A.append(I[i][0])
+        else:
+            A.append(I[i][0])
+            A.append(I[i][1])
+    A.sort()
+    n = len(A)
+    F = [0 for _ in range(n)]
+    for i in range(len(I)):
+        start = I[i][0]
+        end = I[i][1]
+        j = 0
+        while j <= n and A[j] < start:
+            j += 1
+        while j < n and A[j] <= end:
+            F[j] += 1
+            j += 1
+    return max(F)
+
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( snow, all_tests = False )
+runtests(snow, all_tests=True)
