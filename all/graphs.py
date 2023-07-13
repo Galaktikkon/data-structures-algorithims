7de4de32c8a4ef76ecf5df7a23c38fd6bef178cc
Marek Malek <malekmarek56@gmail.com>
2023-07-13 09:41:45 +0200

    dead

--- all/graphs.py
+++ all/graphs.py
@@ -428,3 +428,31 @@ def FordFulkerson(G, s, t):
         update(F, path)
         path = bfs(F, s, t)
     return flow
+
+# DIJKSTRA MATRIX
+
+
+def dijkstra_matrix(G, s):
+    n = len(G)
+    parent = [-1 for _ in range(n)]
+    visited = [False for _ in range(n)]
+    d = [inf for _ in range(n)]
+    d[s] = 0
+
+    while True:
+        weight = inf
+        vert = -1
+        for u in range(n):
+            if not visited[u] and d[u] < weight:
+                weight = d[u]
+                vert = u
+        if vert < 0:
+            break
+        visited[vert] = True
+        for v in range(n):
+            if G[vert][v] == 0 or visited[v] == True:
+                continue
+            if d[vert] + G[vert][v] < d[v]:
+                d[v] = d[vert] + G[vert][v]
+                parent[v] = vert
+    return d
