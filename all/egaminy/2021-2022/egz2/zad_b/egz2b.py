558e778d5fa081830a3ffde48281119f20df6d37
Marek Malek <malekmarek56@gmail.com>
2023-08-22 22:50:32 +0200

    cleanings

--- all/egaminy/2021-2022/egz2/zad_b/egz2b.py
+++ all/egaminy/2021-2022/egz2/zad_b/egz2b.py
@@ -16,24 +16,9 @@ def magic(C):
                         F[i] = max(F[i], F[j]+C[j][0]-C[j][g][0])
                     elif C[j][0] < C[j][g][0] and C[j][g][0]-C[j][0] <= F[j]:
                         F[i] = max(F[i], F[j]-C[j][g][0]+C[j][0])
-                    # print(i, F[i], j)
-    # print(F)
 
     return F[n-1]
 
 
-C = [[2, [5, 1], [1, 6], [1, 8]],
-     [2, [7, 2], [1, 4], [1, 2]],
-     [89, [91, 3], [75, 8], [84, 6]],
-     [8, [6, 4], [10, 6], [7, 5]],
-     [4, [5, 5], [1, 7], [3, 5]],
-     [10, [11, 6], [0, 6], [4, 6]],
-     [1, [0, 7], [0, 7], [6, 7]],
-     [57, [51, 8], [45, 8], [50, 8]],
-     [2, [6, 9], [7, 9], [0, 9]],
-     [6, [3, -1], [8, -1], [1, -1]]]
-
-magic(C)
-
 # zmien all_tests na True zeby uruchomic wszystkie testy
-runtests(magic, all_tests=True)
+runtests(magic, all_tests=False)
