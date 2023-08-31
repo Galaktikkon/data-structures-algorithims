3633f3a279b25723bd3c5556519cf187d8b4e860
Marek Malek <malekmarek56@gmail.com>
2023-08-31 17:45:44 +0200

    intuse

--- all/egaminy/2020-2021/egzamin_2_szablony/zad1/zad1.py
+++ all/egaminy/2020-2021/egzamin_2_szablony/zad1/zad1.py
@@ -1,8 +1,19 @@
 from zad1testy import runtests
 
 
+def get_graph(I, x, y):
+    G = [[] for _ in range(y-x+1)]
+
+    for a, b in I:
+        if a >= x and b <= y:
+            G[a-x].append(b-x)
+
+    return G
+
+
 def intuse(I, x, y):
     """tu prosze wpisac wlasna implementacje"""
+    G = get_graph(I, x, y)
 
     return []
 
@@ -10,5 +21,5 @@ def intuse(I, x, y):
 I = [[3, 4], [2, 5], [1, 3], [4, 6], [1, 4]]
 x = 1
 y = 6
-
-runtests(intuse)
+intuse(I, x, y)
+# runtests(intuse)
